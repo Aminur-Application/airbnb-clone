3f3b6dff56cd7817b587999e18e4e01ff9c1e8ed
Prophecyuntold <aminur999@hotmail.com>
2023-07-01 16:13:12 +0100

    create toggle func to toggle between signup and signin

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -57,6 +57,11 @@ const LoginModal = () => {
    })
   }
 
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal])
+
   const footerContent = (
     <div className='flex flex-col gap-4 mt-3'>
       <hr />
@@ -65,10 +70,10 @@ const LoginModal = () => {
       <div className='text-neutral-500 text-center mt-4 font-light'>
         <div className='justify-center flex flex-row items-center gap-2'>
           <div>
-            Already Have an Account?
+            First time using Airbnb?
           </div>
-          <div onClick={registerModal.onClose} className='text-neutral-800 cursor-pointer hover:underline'>
-            Log in
+          <div onClick={toggle} className='text-neutral-800 cursor-pointer hover:underline'>
+            Create an account
           </div>
         </div>
       </div>
