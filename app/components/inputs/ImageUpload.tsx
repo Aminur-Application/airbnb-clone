9dc2d1fd6037e84f56b9534453d4ad2da93e982d
Prophecyuntold <aminur999@hotmail.com>
2023-07-09 16:43:21 +0100

    update imageUpload component to store multiple images sources

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -3,50 +3,57 @@
 import React, { useCallback } from 'react'
 import { CldUploadWidget } from 'next-cloudinary'
 import Image from 'next/image'
-import { TbPhotoPlus } from 'react-icons/tb'
+import { TbPhotoPlus, TbTrash } from 'react-icons/tb'
+import Button from '../Button'
+import { BiTrash } from 'react-icons/bi'
 
 declare global {
   var cloudinary: any;
 }
 
 interface ImageUploadProps {
-  onChange: (value: string) => void;
-  value: string;
+  onChange: (value: string[]) => void;
+  value: string[];
 }
 
 const ImageUpload: React.FC<ImageUploadProps> = ({
   onChange,
-  value
+  value,
 }) => {
 
   const handleUpload = useCallback((result: any) => {
-    onChange(result.info.secure_url)
-  }, [onChange])
+    const newImageUrl = result.info.secure_url;
+    const updatedValue = [...value]; // Create a copy of the existing value array
+    updatedValue.push(newImageUrl); // Push the new image URL into the copied array
+    onChange(updatedValue);
+  }, [onChange, value]);
+
+  console.log(value)
+
 
   return (
-    <CldUploadWidget
-      onUpload={handleUpload}
-      uploadPreset="gsfzfhds"
-      options={{
-        maxFiles:1
-      }}
-    >
-      {({ open }) => {
-        return (
-          <div onClick={() => open?.()} className='relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600'>
-            <TbPhotoPlus size={50}/>
-            <div className='font-semibold text-lg'>
-              Click to upload
-            </div>
-            {value && (
-              <div className='absolute inset-0 w-full h-full'>
-                <Image alt="Upload" fill style={{ objectFit: 'cover'}} src={value}/>  
+    <>
+      <CldUploadWidget
+        onUpload={handleUpload}
+        uploadPreset="gsfzfhds"
+        options={{
+          maxFiles: 4,
+          multiple: true,
+        }}
+      >
+        {({ open }) => {
+          return (
+            <div onClick={() => open?.()} className='relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600'>
+              <TbPhotoPlus size={50} />
+              <div className='font-semibold text-lg'>
+                Click to upload
               </div>
-            )}
-          </div>
-        )
-      }}
-    </CldUploadWidget>
+            </div>
+          )
+        }}
+      </CldUploadWidget>
+    </>
+
   )
 }
 
