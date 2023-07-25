48d81f27bf1e77b9d292ce71c6f6f37bd2f650d5
Prophecyuntold <aminur999@hotmail.com>
2023-07-25 17:48:34 +0100

    attempt to fix env variable loading issue

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
       "lh3.googleusercontent.com",
       "res.cloudinary.com"
     ]
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
   }
 }
 
