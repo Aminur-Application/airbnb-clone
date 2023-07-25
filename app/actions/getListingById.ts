f2cf5ec77040ed0c683e3e605c903ca7a39d53d1
Prophecyuntold <aminur999@hotmail.com>
2023-07-25 20:38:43 +0100

    return images, fix typescript

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -33,7 +33,8 @@ export default async function getListingById(
         updatedAt: listing.user.updatedAt.toString(),
         emailVerified: 
           listing.user.emailVerified?.toString() || null,
-      }
+      },
+      images: listing.images
     };
   } catch (error: any) {
     throw new Error(error);
